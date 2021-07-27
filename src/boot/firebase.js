d3d1f2b355b6515b3b257b62268d89462714c47c
Lionel Quiambao <lionelquiambao@gmail.com>
2021-07-27 15:53:19 -0700

    Created firebase boot file

---
+++ src/boot/firebase.js
@@ -0,0 +1,27 @@
+// Firebase App (the core Firebase SDK) is always required and must be listed first
+import firebase from "firebase/app";
+// If you are using v7 or any earlier version of the JS SDK, you should import firebase using namespace import
+// import * as firebase from "firebase/app"
+
+// If you enabled Analytics in your project, add the Firebase SDK for Analytics
+import "firebase/analytics";
+
+// Add the Firebase products that you want to use
+import "firebase/auth";
+import "firebase/database";
+
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+var firebaseConfig = {
+  apiKey: "AIzaSyBAM68SmTLCfb7XV0cJizB9uh5M-8R2hM0",
+  authDomain: "functions-1e766.firebaseapp.com",
+  projectId: "functions-1e766",
+  storageBucket: "functions-1e766.appspot.com",
+  messagingSenderId: "1045236061054",
+  appId: "1:1045236061054:web:4ac896d5adb7a33a96192f"
+};
+
+let firebaseApp = firebase.initializeApp(firebaseConfig);
+let firebaseAuth = firebaseApp.auth();
+let firebaseDb = firebaseApp.database();
+
+export { firebaseAuth, firebaseDb };
