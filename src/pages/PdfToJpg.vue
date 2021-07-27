a5676285dc2734a7390ad37c88fb85e39d83c015
Lionel Quiambao <lionelquiambao@gmail.com>
2021-07-27 08:50:42 -0700

    Added the q-file and q-btn needed for the pdf

--- src/pages/PdfToJpg.vue
+++ src/pages/PdfToJpg.vue
@@ -1,15 +1,41 @@
 <template>
-  <q-page class="flex flex-center">
-    <q-btn color="primary">We're Here</q-btn>
+  <q-page class="row justify-center q-pa-md">
+    <div class="column q-gutter-md">
+      <q-file
+        v-model="pdf"
+        label="PDF"
+        label-color="primary"
+        color="grey-3"
+        dark
+      >
+        <template v-slot:append>
+          <q-icon name="attachment" color="primary" />
+        </template>
+      </q-file>
+      <q-btn color="primary" @click="convert">
+        Convert To JPG
+      </q-btn>
+    </div>
   </q-page>
 </template>
 
 <script>
-import { defineComponent } from "vue";
+import { defineComponent, ref } from "vue";
 
 export default defineComponent({
   setup() {
-    return {};
+    const pdf = ref(null);
+    async function convert() {
+      console.error("pdf:", pdf.value);
+      if (pdf.value) {
+        const buffer = await pdf.value.arrayBuffer();
+        console.error("buffer:", buffer);
+      }
+    }
+    return {
+      pdf,
+      convert
+    };
   }
 });
 </script>
