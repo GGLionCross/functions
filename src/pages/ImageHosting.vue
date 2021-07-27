302b4d8eabf90e146098db6dd7a36bc7e83a36ea
Lionel Quiambao <lionelquiambao@gmail.com>
2021-07-27 15:42:33 -0700

    Added the QFile component

--- src/pages/ImageHosting.vue
+++ src/pages/ImageHosting.vue
@@ -1,14 +1,28 @@
 <template>
   <q-page class="flex flex-center">
+    <q-file
+      v-model="images"
+      label="Images"
+      label-color="primary"
+      dark
+      multiple
+    >
+      <template v-slot:append>
+        <q-icon name="attachment" color="primary" />
+      </template>
+    </q-file>
   </q-page>
 </template>
 
 <script>
-import { defineComponent } from "vue";
+import { defineComponent, ref } from "vue";
 
 export default defineComponent({
   setup() {
-    return { };
+    const images = ref(null);
+    return {
+      images
+    };
   }
 });
 </script>
